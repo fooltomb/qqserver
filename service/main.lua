7c77b7bc85233db5865c1063c13608878d2b52fa
fooltomb <fool_tomb@sina.com>
2021-11-25 13:41:08 +0800

    init login service

--- service/main.lua
+++ service/main.lua
@@ -1,6 +1,19 @@
 local skynet = require "skynet"
+
+local runconfig = require "runconfig"
+
 skynet.start(function (  )
+	--初始化
+	local mynode = skynet.getenv("node")
+	local nodecfg = runconfig[mynode]
+
 	skynet.error("[start main]")
 	skynet.newservice("gateway","gateway",1)
+
+	--login
+	for i,v in pairs(nodecfg.login or {}) do
+		local srv = skynet.newservice("login","login",i)
+		skynet.name("login"..i,srv)
+	end
 	skynet.exit()
 end)
