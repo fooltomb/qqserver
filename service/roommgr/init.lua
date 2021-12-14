c32838c4d81e889b91f226650a65ddb3b97b47c0
fooltomb <fool_tomb@sina.com>
2021-12-14 14:14:26 +0800

    test setplayer

---
+++ service/roommgr/init.lua
@@ -0,0 +1,79 @@
+local skynet = require "skynet"
+local s = require "service"
+
+local rooms = {}--[roomid]=room
+local playerRoom = {} --[playerId]=room
+local playerAgent = {} --[playerId]=agent
+
+local function GetRoom( roomName )
+	local m = {
+		id=0,
+		name=roomName,
+		count=0,
+		players={},
+		status="ready"
+	}
+	function m:Join( playerId )
+		if(self.count>=8) then
+			return false,{"room",1,"房间已满"}
+		end
+		self.players[playerId]="Join",
+		self.count=self.count+1,
+		return true,{"room",0,self.id}
+	end
+	function m:Prepare( playerId )
+		self.players[playerId]="Ready"
+		local readyCount = 0
+		for k,v in pairs(self.players) do
+			if(v=="Ready") then
+				readyCount=readyCount+1
+			end
+		end
+		if readyCount==self.count then
+			--to do 
+		end
+	end
+	function m:CancelPrepare( playerId )
+		self.players[playerId]="Join"
+	end
+	function m:Exit( playerId )
+		self.players[playerId]=nil
+		self.count=self.count-1
+		if self.count<=0 then
+			--kill room
+		end
+	end
+	return m
+end 
+
+s.resp.CreateRoom=function ( source,playerid,agent,roomName )
+	local room = GetRoom(roomName)
+	local isok=false
+	for i=1,100 do
+		if rooms[i]==nil then
+			room.id=i
+			rooms[i]=room
+			isok = true
+			break
+		end
+	end
+	if not isok then 
+		return {"room",1,"房间数量已达上限"}
+	end
+	local joinok,ret = 1 room:Join(playerid)
+	if joinok then
+		playerRoom[playerid]=room
+		playerAgent[playerid]=agent
+	end
+	return ret
+end
+
+s.resp.GetRoomList=function ( source )
+	local msg = ""
+	for k,v in pairs(rooms) do
+		msg=msg..v.id..":"..v.name..";"
+	end
+	return {"roomList",0,msg}
+end
+
+s.start(...)
